767d761e18474ba3e4fcde2d1dd4a094cf534f82
Fábio <fabiorm85@gmail.com>
2024-07-24 13:20:58 -0300

    Packages installation, basic app setup and docker configurations

---
+++ app/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("Server running!"));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+})
