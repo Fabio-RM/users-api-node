fdd44592ab6de55078880f54ddfde13007424ac7
Fábio <fabiorm85@gmail.com>
2024-09-05 12:14:38 -0300

    feat: minor ajustments on cors config

--- app/src/config/cors.ts
+++ app/src/config/cors.ts
@@ -2,6 +2,7 @@ const corsOptions = {
     origin: 'http://localhost:5174',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }
 
 export default corsOptions
